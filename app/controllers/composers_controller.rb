ae8999102551fb95ea6c58bff0e47b218a06aa0e
Boolean <s.boolean@gmail.com>
2009-03-23 20:50:35 +0000

    new views for Composer Controller. Include new, show html's files

--- app/controllers/composers_controller.rb
+++ app/controllers/composers_controller.rb
@@ -2,16 +2,51 @@ class ComposersController < ApplicationController
   def index
     @composers = Composer.find(:all)
 
-     respond_to do |format|
-       format.html # index.html.erb
-       format.xml { render :xml => @composers }
-     end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @composers }
+    end
   end
 
   def new
+    @composer = Composer.new
+
+    respond_to do |format|
+      format.html # edit.html.erb
+      format.xml { render :xml }
+    end
   end
 
   def edit
+    respond_to do |format|
+      format.html # edit.html.erb
+      format.xml { render :xml }
+    end
   end
 
+  def show
+    @composer = Composer.find(params[:id])
+
+    respond_to do |format|
+      format.html # edit.html.erb
+      format.xml { render :xml => @composer}
+    end
+  end
+
+  def create
+    @composer = Composer.new(params[:composer])
+
+    respond_to do |format|
+      if @composer.save
+        flash[:notice] = 'Composer was successfully created.'
+        format.html { redirect_to(@composer) }
+        format.xml  { render :xml => @composer, :status => :created,
+                    :location => @composer }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @composer.errors,
+                    :status => :unprocessable_entity }
+      end
+    end
+  end
 end
