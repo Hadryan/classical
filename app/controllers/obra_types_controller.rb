dafbb12098c26d40899be80d18c77d4b831ea65b
Boolean <s.boolean@gmail.com>
2009-03-25 15:21:16 +0000

    new views for ObraType Controller. Include new, show html's files

--- app/controllers/obra_types_controller.rb
+++ app/controllers/obra_types_controller.rb
@@ -1,8 +1,12 @@
 class ObraTypesController < ApplicationController
+  before_filter :find_obra_type,  :only => [:show, :edit, :update, :destroy]
+  
   def index
+    @obraTypes = ObraType.find(:all)
   end
 
   def new
+    @obraType = ObraType.new
   end
 
   def edit
@@ -11,4 +15,47 @@ class ObraTypesController < ApplicationController
   def show
   end
 
+  def create
+    @obraType = ObraType.new(params[:obra_type])
+
+    respond_to do |format|
+      if @obraType.save
+        flash[:notice] = 'ObraType was successfully created.'
+        format.html { redirect_to(@obraType) }
+        format.xml  { render :xml => @obraType, :status => :created,
+                    :location => @obraType }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @obraType.errors,
+                    :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @obraType.update_attributes(params[:obra_type])
+        flash[:notice] = 'ObraType was successfully updated.'
+        format.html { redirect_to(@obraType) }
+        format.xml { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml { render :xml => @obraType.errors,  :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @obraType.destroy
+
+     respond_to do |format|
+       format.html { redirect_to(obra_types_url) }
+       format.xml { head :ok }
+     end
+  end
+
+  private
+    def find_obra_type
+      @obraType = ObraType.find(params[:id])
+    end
 end
--- app/models/obra_type.rb
+++ app/models/obra_type.rb
@@ -1,2 +1,3 @@
 class ObraType < ActiveRecord::Base
+  validates_presence_of :name
 end
