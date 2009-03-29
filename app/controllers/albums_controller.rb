30ea1d866bf638cb73aec896bf44046ca855ef41
Boolean <s.boolean@gmail.com>
2009-03-29 22:01:06 +0000

    Skeleton for albums, without the associations

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,14 +1,61 @@
 class AlbumsController < ApplicationController
-  def show
-  end
+  before_filter :find_album,  :only => [:show, :edit, :update, :destroy]
 
   def index
+    @albums = Album.find(:all)
   end
 
   def new
+    @album = Album.new
   end
 
   def edit
   end
 
+  def show
+  end
+
+  def create
+    @album = Album.new(params[:album])
+
+    respond_to do |format|
+      if @album.save
+        flash[:notice] = 'Album was successfully created.'
+        format.html { redirect_to(@album) }
+        format.xml  { render :xml => @album, :status => :created,
+                    :location => @album }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @album.errors,
+                    :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @album.update_attributes(params[:album])
+        flash[:notice] = 'Album was successfully updated.'
+        format.html { redirect_to(@album) }
+        format.xml { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml { render :xml => @album.errors,  :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @album.destroy
+
+     respond_to do |format|
+       format.html { redirect_to(albums_url) }
+       format.xml { head :ok }
+     end
+  end
+
+  private
+    def find_album
+      @album = Album.find(params[:id])
+    end
 end
