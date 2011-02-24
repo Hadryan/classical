e27464418f0f47be5c79a6b8bccacd00f3354599
Fernando Saenz <s.boolean@gmail.com>
2011-02-24 18:54:56 -0300

    removed useless file

--- rails_admin.rb
+++
@@ -1,9 +0,0 @@
-require "rails_admin/application_controller"
-
-module RailsAdmin
-  class ApplicationController < ::ApplicationController
-    access_control do
-      allow :admin
-    end
-  end
-end
